ccb03f0e0d20a6fb6deeb779cc5bed1115a55d15
AlbertoCorona <muqui@hotmail.com>
2019-02-10 12:55:28 -0600

    cambio base

--- src/main/java/com/muqui/corona/configuration/HibernateConfiguration.java
+++ src/main/java/com/muqui/corona/configuration/HibernateConfiguration.java
@@ -19,7 +19,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableTransactionManagement
 @ComponentScan({ "com.muqui.corona.configuration" })
-@PropertySource(value = { "classpath:application.properties" })
+@PropertySource(value = { "classpath:applicationheroku.properties" })
 public class HibernateConfiguration {
 
     @Autowired
